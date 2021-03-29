476f51e44ee2a7a79f7c97b4355f83576bc80d06
zainmousa958 <80972355+zainmousa958@users.noreply.github.com>
2021-03-29 16:04:48 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,7 +2,7 @@
 
 int gcd(int a, int b) {
                 int res;
-        while ((b != 0) && (a != 0)) {
+        while ((a != 0) && (b != 0)) {
                 if (a > b)
         a -= b;
                 else
