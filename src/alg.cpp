244f877e1d17771ce6ec6acff7e73595086f76a0
zainmousa958 <80972355+zainmousa958@users.noreply.github.com>
2021-03-29 16:01:12 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,13 +1,13 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-                int result;
+                int res;
         while ((b != 0) && (a != 0)) {
                 if (a > b)
         a -= b;
                 else
         b -= a;
         }
-        result = a + b;
-                return result;
+        res = a + b;
+                return res;
         }
